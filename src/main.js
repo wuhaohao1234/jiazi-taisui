51118a64124cff0d79c2995f7162a928fdfb24ad
wuhaohao <1611499758@qq.com>
2026-02-10 12:52:11 +0800

    build hash

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import { createApp } from 'vue'
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import App from './App.vue'
 import './style.css'
 
@@ -11,7 +11,7 @@ import About from './views/About.vue'
 
 // 创建路由
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes: [
     {
       path: '/',

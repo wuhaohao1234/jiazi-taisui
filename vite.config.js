e6e0cfcf6415224be85c5c4b727d0187f0451bdc
wuhaohao <1611499758@qq.com>
2026-02-10 12:47:55 +0800

    build docs

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: '/jiazi-taisui/',
+  build: {
+    outDir: 'docs'
+  }
 })
